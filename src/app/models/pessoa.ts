42e66cc5921acb886f78f6a70f314be7d46655f0
Cleber Marcos <cleberjunior_11@hotmail.com>
2021-01-26 22:02:00 -0300

    padrão incial de inclusão e lista

---
+++ src/app/models/pessoa.ts
@@ -0,0 +1,9 @@
+export class Pessoa {
+
+    constructor(
+      public id: number,
+      public name: string,
+      public alterEgo?: string
+    ) {  }
+  
+}
--- src/app/pessoas-incluir/pessoas-incluir.component.ts
+++ src/app/pessoas-incluir/pessoas-incluir.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Pessoa } from '../models/pessoa';
 
 @Component({
   selector: 'app-pessoas-incluir',
   templateUrl: './pessoas-incluir.component.html',
   styleUrls: ['./pessoas-incluir.component.scss']
 })
+
 export class PessoasIncluirComponent implements OnInit {
+  
+  submitted = false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  onSubmit() {
+     this.submitted = true; 
+     console.info('teste');
+    
+    }
 
 }
